57e24245ea06f0f233b197dd5dacc368f849c988
User850413 <csyou1228@gmail.com>
2025-02-17 20:10:56 +0900

    02/17 :sparkles: : 버튼 컴포넌트 제작

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,47 @@
+import clsx from 'clsx'
 import { MouseEventHandler } from 'react'
 
 export interface ButtonProps {
   text?: string
   onClick?: MouseEventHandler<HTMLButtonElement>
+  type?: 'primary' | 'secondary' | 'tertiary' | 'default' | 'disabled'
+  classNames?: string
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl'
 }
 
 export default function Button({
   text = 'button',
   onClick = () => {},
+  type = 'default',
+  size = 'md',
+  classNames,
 }: ButtonProps) {
-  return <button onClick={onClick}>{text}</button>
+  return (
+    <button
+      onClick={onClick}
+      className={clsx(
+        {
+          'bg-primary hover:bg-primary-darker text-white': type === 'primary',
+          'bg-secondary hover:bg-secondary-darker text-white':
+            type === 'secondary',
+          'bg-tertiary text-primary hover:bg-tertiary-darker':
+            type === 'tertiary',
+          'text-primary bg-gray-300 hover:bg-gray-400': type === 'default',
+          'cursor-default bg-gray-300 text-gray-400': type === 'disabled',
+        },
+        'h-fit',
+        {
+          'h-fit rounded-md px-2 py-1 text-xs': size === 'xs',
+          'rounded-md px-3 py-2 text-sm': size === 'sm',
+          'text-md rounded-md px-4 py-2': size === 'md',
+          'rounded-lg px-4 py-2 text-lg': size === 'lg',
+          'rounded-lg px-4 py-2 text-xl': size === 'xl',
+          'rounded-xl px-5 py-3 text-2xl': size === '2xl',
+        },
+        classNames
+      )}
+    >
+      {text}
+    </button>
+  )
 }
--- src/pages/components-pages/button-page.tsx
+++ src/pages/components-pages/button-page.tsx
@@ -1,9 +1,18 @@
 import Button from '@/components/Button'
 
 export default function ButtonPage() {
+  const alertMessage = () => {
+    window.alert('alert!')
+  }
+
   return (
-    <div>
-      <Button />
+    <div className="m-5 flex gap-4">
+      <Button size="xs" type="primary" text="alert" onClick={alertMessage} />
+      <Button size="sm" type="secondary" />
+      <Button size="md" type="tertiary" />
+      <Button size="lg" type="default" />
+      <Button size="xl" type="disabled" />
+      <Button size="2xl" type="primary" />
     </div>
   )
 }
