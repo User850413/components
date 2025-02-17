95292907a0e794b206c40c1247247f0eb83c07c0
User850413 <csyou1228@gmail.com>
2025-02-17 16:20:45 +0900

    02/17 :sparkles: : button 스토리북 세팅

--- src/pages/components/button.tsx
+++ src/pages/components/button.tsx
@@ -1,7 +1,10 @@
-export interface ButtonProps {
+import { MouseEventHandler } from "react";
+
+export interface ButtonProps extends HTMLButtonElement {
   text: string;
+  onClick: MouseEventHandler<HTMLButtonElement>;
 }
 
-export default function Button({ text }: ButtonProps) {
-  return <div>{text}</div>;
+export default function Button({ text, onClick }: ButtonProps) {
+  return <button onClick={onClick}>{text}</button>;
 }
--- src/pages/components/stories/button.stories.ts
+++ src/pages/components/stories/button.stories.ts
@@ -1,9 +1,13 @@
-import { StoryObj } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import Button from "../button";
+import { fn } from "@storybook/test";
 
 const meta = {
   component: Button,
-};
+  args: { onClick: fn() },
+  parameters: { layout: "centered" },
+  tags: ["autodocs"],
+} satisfies Meta<typeof Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
