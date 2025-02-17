11549e329caf24dbef95e3f939def00f03b17cb8
User850413 <csyou1228@gmail.com>
2025-02-17 19:26:27 +0900

    02/17 :package: : prettier 및 테일윈드 클래스명 정렬 플러그인 설치

--- src/pages/components/button.tsx
+++ src/pages/components/button.tsx
@@ -1,10 +1,14 @@
-import { MouseEventHandler } from "react";
+import { MouseEventHandler } from 'react'
 
 export interface ButtonProps extends HTMLButtonElement {
-  text: string;
-  onClick: MouseEventHandler<HTMLButtonElement>;
+  text: string
+  onClick: MouseEventHandler<HTMLButtonElement>
 }
 
 export default function Button({ text, onClick }: ButtonProps) {
-  return <button onClick={onClick}>{text}</button>;
+  return (
+    <button onClick={onClick} className="relative w-10 p-3">
+      {text}
+    </button>
+  )
 }
